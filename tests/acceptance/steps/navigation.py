e043a4d399bc52212129da2d8057d342adad54e2
Victor Marreiros <v.marreiros@aluno.ifsp.edu.br>
2020-09-25 11:29:00 -0300

    Written the first scenario of navigation.

--- tests/acceptance/steps/navigation.py
+++ tests/acceptance/steps/navigation.py
@@ -6,5 +6,5 @@ use_step_matcher('re')   # permite que nossos steps recebam argumentos do scenar
 
 @given('I am on the homepage')
 def step_impl(context):
-    browser = webdriver.Chrome()
+    browser = webdriver.Chrome("")
     browser.get('http://127.0.0.1:5000')
